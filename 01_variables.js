455db8431b168b15920eb329371a673ff2aae217
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-09 15:44:04 +0530

    index, variable.ts  to variable.js  file converting

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+var num1 = 10;
+console.log(num1);
+var s1 = "You are a creator of your destiny";
+var s2 = "Powerful people makes places powerful";
+console.log(s1);
+console.log(s2);
+var b1 = true;
+var b2 = false;
+var b3;
+console.log(b1);
+console.log(b2);
+function checkBoolean() {
+    console.log(b3);
+}
+checkBoolean();
---
+++ 01_variables.ts
@@ -0,0 +1,21 @@
+
+var num1: number= 10;
+console.log(num1);
+
+var s1 : string = "You are a creator of your destiny";
+var s2 : string = `Powerful people makes places powerful`;
+console.log(s1);
+console.log(s2);
+
+var b1: boolean = true;
+var b2: boolean = false;
+var b3: boolean;
+
+console.log(b1);
+console.log(b2);
+
+function checkBoolean() {
+    console.log(b3);
+}checkBoolean() 
+
+
