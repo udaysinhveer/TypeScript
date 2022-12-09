8a6fb468f78f8438b0d6b8e59302f736f23f06e2
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-09 19:25:35 +0530

    string in TypeScript

---
+++ 04_stringType.js
@@ -0,0 +1,6 @@
+var userName = "Codemind Technology";
+var s1 = "My name is ".concat(userName);
+console.log(s1);
+console.log(s1.indexOf('n'));
+console.log(s1.lastIndexOf('n'));
+console.log(s1.charAt(0));
