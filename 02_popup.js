94705c98f47de151c98ceabeecf8c3aaa20274e8
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-09 19:24:33 +0530

    PopUP

---
+++ 02_popup.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+alert("Hello");
+confirm("Do you rally want to do this?");
+var data = prompt('Please enter your name');
+console.log(data);
---
+++ 02_popup.ts
@@ -0,0 +1,8 @@
+console.log("Hello");
+
+
+alert("Hello");
+confirm("Do you rally want to do this?");
+var data = prompt('Please enter your name');
+
+console.log(data);
