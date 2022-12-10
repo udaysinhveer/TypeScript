b622212efae39d31e19c624554f4e889033f0f1e
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-10 09:28:25 +0530

    object

---
+++ 06_object.js
@@ -0,0 +1,8 @@
+var emp = {
+    name: "Uday",
+    score: 250
+};
+for (var empUday in emp) {
+    console.log(empUday);
+    //    console.log(emp[empUday]);
+}
---
+++ 06_object.ts
@@ -0,0 +1,10 @@
+var emp = {
+    name: "Uday",
+    score : 250,
+}
+
+for (const empUday in emp) {
+   console.log(empUday);
+//    console.log(emp[empUday]);
+   
+}
