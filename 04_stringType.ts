111eedc4a03fd2b7f0e7e5e259be6d77e7ecb27a
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-17 07:31:42 +0530

    Update 04_stringType.ts

--- 04_stringType.ts
+++ 04_stringType.ts
@@ -2,8 +2,8 @@ var userName : string = "Codemind Technology";
 var s1 = `My name is ${userName}`;
 console.log(s1);
 
-console.log(s1.indexOf('n'));
-console.log(s1.lastIndexOf('n'));
+console.log(s1.indexOf('n'));   // it will return first occurrence of "n"
+console.log(s1.lastIndexOf('n')); // it will return last occurrence of "n"
 console.log(s1.charAt(0));
 
 
