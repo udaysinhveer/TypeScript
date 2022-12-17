a2eee22940c4c9a0134f81eb2fa073165fba2363
Udaysinh Veer <udaysinhveer3@gmail.com>
2022-12-17 07:31:11 +0530

    enum

--- 03_enum.js
+++ 03_enum.js
@@ -1,3 +1,4 @@
+// enum is a sequence of constant values
 var Gender;
 (function (Gender) {
     Gender[Gender["Male"] = 0] = "Male";
@@ -5,6 +6,9 @@ var Gender;
 })(Gender || (Gender = {}));
 console.log(Gender.Male);
 console.log(Gender.Female);
+// another way to access
+console.log(Gender[0]);
+console.log(Gender[1]);
 var Weekends;
 (function (Weekends) {
     Weekends[Weekends["saturday"] = 6] = "saturday";
--- 03_enum.ts
+++ 03_enum.ts
@@ -1,9 +1,18 @@
+
+// enum is a sequence of constant values
+
 enum Gender{
     Male,
     Female,
 }
-console.log(Gender.Male);
-console.log(Gender.Female);
+console.log(Gender.Male);  // it will return 0
+console.log(Gender.Female); // it will return 1
+
+// another way to access
+console.log(Gender[0]); // it will return Male,
+console.log(Gender[1]); // it will return Female,
+
+
 
 enum Weekends {
     saturday = 6,
